87fcdd072a3537b19e32756822cb544c5a4df7cb
ssarnobat <ss401533@gmail.com>
2016-08-23 20:07:27 -0700

    Minor changes (untested)

--- htmlify.sh
+++ htmlify.sh
@@ -1,4 +1,1 @@
-http://netgear.rohidekar.com:44452.
-http://netgear.rohidekar.com:44452./sh
-http://netgear.rohidekar.com:44452./README.md
-http://netgear.rohidekar.com:44452./htmlify.sh
+perl -pe 's{^(.*)$}{<img src="$1" height=150><br>}g'

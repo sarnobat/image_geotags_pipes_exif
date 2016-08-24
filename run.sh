de488c65e64d009a663a4e43ad9aae13fa2a500c
ssarnobat <ss401533@gmail.com>
2016-08-23 20:41:02 -0700

    Minor changes (untested)

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 find /Unsorted/new/Photos/ -iname "*jpg" | head -10 | sh httpify.sh | sh htmlify.sh
+
+#mkfifo resulta resultb
 #seq 4 | tee  >(perl -pe 's{^(.*)$}{(print image tag for $1)}g' > resulta) >(perl -pe 's{^(.*)$}{(print geotag for $1)}g' > resultb) | paste -d ' ' - resulta resultb

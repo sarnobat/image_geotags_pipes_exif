e2c6cd333625722c8120ed525dd7180b75b4bdb5
ssarnobat <ss401533@gmail.com>
2016-08-25 21:43:01 -0700

    Minor changes (untested)

--- htmlify.sh
+++ htmlify.sh
@@ -1,1 +1,9 @@
-perl -pe 's{^(.*)$}{<img src="$1" height=150><br>}g'
+#xargs --delimiter "\n" -n 1 sh geotag.sh
+ARG="$@"
+OUT=`echo $ARG | perl -pe 's{^(.*)$}{<img src="$1" height=150><br>}g')`
+OUT2=`echo $ARG | perl -pe 's{.*44452}{}g' | xargs -d "\n"  -n 1 sh geotag.sh`
+
+#OUT2=i`sh geotag.sh $ARG`
+echo $OUT
+echo $OUT2
+echo "<br><br>"
--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
-find /Unsorted/new/Photos/ -iname "*jpg" | head -10 | sh httpify.sh | sh htmlify.sh
+mkfifo resulta resultb
+find /Unsorted/new/photos/iPhone -iname "*JPG" | grep 17  | tee  >(sh httpify.sh | xargs -n 1 -d "\n" sh htmlify.sh > resulta) | paste -d ' ' resulta  | tee index.html
+#find /Unsorted/new/photos/iPhone -iname "*JPG" | head -5 | tee  >(sh httpify.sh | sh htmlify.sh > resulta) >(xargs --delimiter "\n" -n 1 sh geotag.sh > resultb) | paste -d ' ' resulta resultb | tee index.html
 
-#mkfifo resulta resultb
+#find /Unsorted/new/Photos/ -iname "*jpg" | head -10 | sh httpify.sh | sh htmlify.sh
+
 #seq 4 | tee  >(perl -pe 's{^(.*)$}{(print image tag for $1)}g' > resulta) >(perl -pe 's{^(.*)$}{(print geotag for $1)}g' > resultb) | paste -d ' ' - resulta resultb

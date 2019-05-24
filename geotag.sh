3561fda7c93d38617b3175fbb457dad2c9bffeee
sarnobat <ss401533@gmail.com>
2019-05-24 14:50:31 -0700

    Update geotag.sh

--- geotag.sh
+++ geotag.sh
@@ -1,1 +1,2 @@
-exiftool -q -n -p '$GPSLatitude,$GPSLongitude' "$@" | xargs  -I% curl -s 'https://maps.googleapis.com/maps/api/geocode/json?latlng='%'&sensor=true&key=AIzaSyA6mE4febQ1RlD_rhtPaqVX3aLlIGcx5Cw'| jq --unbuffered '.results[0].formatted_address'
+# The api key is AIzaSyA************Gcx5Cw
+exiftool -q -n -p '$GPSLatitude,$GPSLongitude' "$@" | xargs  -I% curl -s 'https://maps.googleapis.com/maps/api/geocode/json?latlng='%'&sensor=true&key='$API_KEY| jq --unbuffered '.results[0].formatted_address'
